84f20dfc4342d05a695dbaacf13849916adb3b3d
Karl Lehenbauer <karl@flightaware.com>
2021-05-09 13:37:25 +0000

    start reworking for tohil

--- setup.py
+++ setup.py
@@ -10,20 +10,20 @@ with io.open('README.rst', encoding='utf-8') as readme_file:
 
 
 setup(
-    name='python-docs-theme',
+    name='tohil-docs-theme',
     # Version is date based as year.month[.serial], where serial is used
     # if multiple releases are needed to address build failures.
     version='2021.5.dev',
     description='The Sphinx theme for the CPython docs and related projects',
     long_description=long_description,
-    author='PyPA',
-    author_email='distutils-sig@python.org',
-    url='https://github.com/python/python-docs-theme/',
-    packages=['python_docs_theme'],
+    author='FlightAware Developers',
+    author_email='noreply@flightaware.com',
+    url='https://github.com/flightaware/tohil-docs-theme/',
+    packages=['tohil_docs_theme'],
     include_package_data=True,
     entry_points={
         'sphinx.html_themes': [
-            'python_docs_theme = python_docs_theme',
+            'tohil_docs_theme = tohil_docs_theme',
         ]
     },
     classifiers=[
